100fa2aa58b22aa6c874bda12b9e662b2ba99b2f
ScatDev <scatdevelopment@gmail.com>
2023-04-16 07:19:00 -0400

    added alerts command and allowed custom punish commands

---
+++ src/main/java/dev/scat/aquarium/command/AlertsCommand.java
@@ -0,0 +1,46 @@
+package dev.scat.aquarium.command;
+
+import dev.scat.aquarium.Aquarium;
+import dev.scat.aquarium.config.Config;
+import dev.scat.aquarium.data.PlayerData;
+import dev.scat.aquarium.util.ColorUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+
+public class AlertsCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof ConsoleCommandSender) {
+            sender.sendMessage(Config.PLAYER_ONLY_MESSAGE.translate());
+            return false;
+        }
+
+        if (!sender.hasPermission("aquarium.alerts")) {
+            sender.sendMessage(Config.NO_PERMISSION_MESSAGE.translate());
+        }
+
+        if (args.length != 0) {
+            sender.sendMessage(ColorUtil.translate("&cWrong usage, please use : /alerts"));
+            return false;
+        }
+
+        PlayerData data = Aquarium.getInstance().getPlayerDataManager().get((Player) sender);
+
+        if (data.isAlerting()) {
+            data.setAlerting(false);
+
+            sender.sendMessage(Config.ALERTS_DISABLED_MESSAGE.translate());
+        } else {
+            data.setAlerting(true);
+
+            sender.sendMessage(Config.ALERTS_ENABLED_MESSAGE.translate());
+        }
+
+        return true;
+    }
+}
